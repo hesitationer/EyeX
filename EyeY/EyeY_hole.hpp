bf756e9535665b75433f86daf1aab870b10e9d90
ZHONG SIFEN <zhongsifen@gmail.com>
2016-06-09 16:49:00 +0800

    with fga framework

--- EyeY/EyeY_hole.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "_EyeY.hpp"
-
-extern EyeY* Hole;
---
+++ _EyeX.hpp
@@ -0,0 +1,14 @@
+//
+//  _EyeX.hpp
+//  EyeX
+//
+//  Created by SIFEN ZHONG on 24/8/15.
+//
+//
+
+#ifndef _EyeX_EyeX_hpp
+#define _EyeX_EyeX_hpp
+
+#include "EyeX.hpp"
+
+#endif
